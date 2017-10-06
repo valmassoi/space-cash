73abe0243d1f87ca5553a1756566edf4fc17b5f2
Robert Valmassoi <rvalmassoi@gmail.com>
2017-10-06 11:34:38 -0600

    cluster logging

--- cluster.js
+++ cluster.js
@@ -1,6 +1,8 @@
 const cluster = require('cluster')
 
 if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`)
+
   // Count the machine's CPUs
   let cpuCount = require('os').cpus().length
 
@@ -10,9 +12,11 @@ if (cluster.isMaster) {
   }
 
   // Listen for dying workers
-  cluster.on('exit', () => {
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`)
     cluster.fork()
   })
 } else {
+  console.log(`Worker ${process.pid} started`)
   require('./server')
 }
